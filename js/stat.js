dc8c8356f6fcfb017cf7052ad3e1b23deb033c43
niksolid <semenov.send@gmail.com>
2020-09-24 19:51:17 +0300

    настраивает канвас со статистикой игроков

--- js/stat.js
+++ js/stat.js
@@ -1,23 +1,26 @@
 'use strict';
 
-const CLOUD_WIDTH = 500;
-const CLOUD_HEIGHT = 200;
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
 const CLOUD_X = 100;
 const CLOUD_Y = 10;
 const GAP = 10;
-const TEXT_WIDTH = 75;
 const BAR_WIDTH = 40;
 const BAR_HEIGHT = 150;
 const BAR_PADDING = 50;
 
-
-const renderCloud = (ctx, x, y, color) => {
+const renderCloudElement = (ctx, x, y, color) => {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+const renderCloud = (ctx) => {
+  renderCloudElement(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloudElement(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+};
+
 const getMaxElement = (arr) => {
-  const maxElement = arr[0];
+  let maxElement = arr[0];
 
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
@@ -28,29 +31,33 @@ const getMaxElement = (arr) => {
   return maxElement;
 };
 
-
-window.renderStatistics = function (ctx, names, times) {
-
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-
+const renderText = (ctx) => {
   ctx.fillStyle = '#000000';
-
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-
   ctx.fillText('Ура вы победили', CLOUD_X + GAP, GAP * 4);
   ctx.fillText('Список результатов:', CLOUD_X + GAP, GAP * 6);
-
-  // const maxTimes = getMaxElement(times);
-
-  for (let i = 0; i < names.length; i++) {
-    ctx.fillText(names[i], CLOUD_X + GAP * 4, CLOUD_Y + GAP);
-    ctx.fillRect(CLOUD_X + GAP * 4 + (BAR_WIDTH + BAR_PADDING) * i, CLOUD_HEIGHT, BAR_WIDTH, BAR_HEIGHT);
-  }
-
-
-  console.log(names);
-  console.log(times);
 };
 
+const renderPlayersResult = (ctx, names, times) => {
+  const maxTime = getMaxElement(times);
+
+  for (let i = 0; i < names.length; i++) {
+    const playerTime = Math.round(BAR_HEIGHT * times[i] / maxTime);
+    const columnX = CLOUD_X + GAP * 4 + (BAR_WIDTH + BAR_PADDING) * i;
+    const colorSaturation = `hsl(240, ${Math.round(Math.random() * 100) + '%'}, 50%)`;
+    ctx.fillStyle = '#000000';
+    ctx.fillText(names[i], columnX, CLOUD_HEIGHT - GAP);
+    ctx.fillStyle = colorSaturation;
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    }
+    ctx.fillRect(columnX, CLOUD_HEIGHT - GAP * 3 - playerTime, BAR_WIDTH, playerTime);
+  }
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx);
+  renderText(ctx);
+  renderPlayersResult(ctx, names, times);
+};
